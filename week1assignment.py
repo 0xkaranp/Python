993b1965641e4d8fe352883442d05214a99622d2
0xkaranp <107405481+0xkaranp@users.noreply.github.com>
2025-06-07 13:06:42 +0530

    Week 1

---
+++ week1assignment.py
@@ -0,0 +1,27 @@
+#Question 1
+
+#a = input("Enter Your Name ")
+#b = input("Enter YOur Age ")
+#print(f"Your Name is {a}and Age is {b}")
+
+#Question 2
+
+#d = int(input("Enter A Value"))
+#e = int(input("Enter another value"))
+
+
+#print(f"The addition is{d+e}")
+#print(f"The subtraction is {d-e}")
+#print(f"The multiplication is{d*e}")
+#print(f"The division is{d/e}")
+
+#Question 3
+#kilo = int(input("Enter Your kilometers"))
+#print(f"The kilometers to miles is {kilo*0.621371}")
+
+#Question 4
+num = int(input("Enter a number "))
+if num % 2 == 0:
+    print("It Is Even Number")
+else:
+    print("It is odd Number")
